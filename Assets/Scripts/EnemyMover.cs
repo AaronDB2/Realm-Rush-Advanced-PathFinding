1c5bde0b3b937e9b24dc7d9a6d7571c4be3f666d
Aaron de Boer <aarondeboerwk@gmail.com>
2023-08-22 17:44:31 +0200

    Added assets and made enemy movement faster and more smooth

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,18 +9,37 @@ public class EnemyMover : MonoBehaviour
     [Tooltip("List of waypoints to follow")]
     List<Waypoint> path = new List<Waypoint>();
 
+    [SerializeField]
+    [Tooltip("How fast the enemy moves")]
+    [Range(0f, 5f)]
+    float speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
     }
 
-    // Function for printing the full path name into the console
-    void PrintWaypointName() 
-    { 
-        foreach(Waypoint waypoint in path)
+    // Coroutine Function for letting object follow the path
+    IEnumerator FollowPath() 
+    {
+		foreach (Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            // Run while travelPercent is not 100%
+            while(travelPercent < 1f) 
+            {
+                travelPercent += Time.deltaTime * speed;
+
+				transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+			}
+
         }
     }
 }
